9e9e4a1fd07eb8536b089e0a2ac52d8f9aaa2b94
Infinite Spiral <stephaneramael@live.co.uk>
2024-11-10 14:24:09 +0000

    sanityse te comment input, adding eslint security, nodemon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
+'use strict';
+
 module.exports = {
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:security/recommended-legacy',
+  ],
   parserOptions: {
     ecmaVersion: 12,
   },
+  plugins: [
+    'security',
+  ],
   rules: {
-    strict: ["global"],
+    strict: ["error","global"],
   },
 };
